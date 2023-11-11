0e16924165aa1c83812b706865976be397486e21
ritesh-hehe <150435159+ritesh-hehe@users.noreply.github.com>
2023-11-11 19:42:18 +0530

    Added reverse param for selectionSort
    Copy Pasted a bunch o' stuff...just how the forefathers intended it to be.

--- small-programs/dsa/algorithms/sorting/selection_sort.py
+++ small-programs/dsa/algorithms/sorting/selection_sort.py
@@ -1,13 +1,21 @@
 DEMO_LIST = [1, 1, 2, 5, 3, 7, 4, 11, 23, 109, 37, 54, 12, 109, 12]
 
 
-def selectionSort(sort_list: list) -> list:
-    for i in range(len(DEMO_LIST)):
-        for j in range(len(DEMO_LIST)):
-            if sort_list[i] < sort_list[j]:
-                sort_list[i], sort_list[j] = sort_list[j], sort_list[i]
-    return sort_list
+def selectionSort(sort_list: list, reverse=False) -> list:
+    if not reverse:
+        for i in range(len(DEMO_LIST)):
+            for j in range(len(DEMO_LIST)):
+                if sort_list[i] < sort_list[j]:
+                    sort_list[i], sort_list[j] = sort_list[j], sort_list[i]
+        return sort_list
+    else:
+        for i in range(len(DEMO_LIST)):
+            for j in range(len(DEMO_LIST)):
+                if sort_list[i] > sort_list[j]:
+                    sort_list[i], sort_list[j] = sort_list[j], sort_list[i]
+        return sort_list
 
 
 print(f"Before Sort -> {DEMO_LIST}")
 print(f"After Sort -> {selectionSort(DEMO_LIST)}")
+print(f"Descending -> {selectionSort(DEMO_LIST, reverse=True)}")
